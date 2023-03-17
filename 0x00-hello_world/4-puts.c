1c173c2d62a6d8dbc92563199233993b7692d5df
Emmanuel Dogbey <dogbeyemma@gmail.com>
2023-03-16 17:49:09 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - print a line of code with puts
+ * main - Entry point
  *
- * return: 0
-*/ 
+ * Return: Always 0(Success)
+ */
 int main(void)
-{ 
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0):
+	return (0);
 }
